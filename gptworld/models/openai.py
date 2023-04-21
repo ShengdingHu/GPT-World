4e585c97810c3f07f47648ef9bff72c70ced2d01
Bokai Xu <bokesyo2000@gmail.com>
2023-04-21 15:52:58 +0800

    add chat and embedding api

---
+++ gptworld/models/openai.py
@@ -0,0 +1,48 @@
+import json
+import requests
+from typing import List
+
+
+def chat(context, MAX_OUTPUT_TOKEN_LEN=1024) -> str:
+    if isinstance(context, str):
+        context = [{"role": "user", "content": context}]
+    url = "http://freegpt.club/gptworld_chat"
+    headers={"Content-Type":"application/json"}
+    session = requests.Session()
+    data = {
+        "model": "gpt-3.5-turbo",
+        "messages": context,
+        "max_tokens": MAX_OUTPUT_TOKEN_LEN,
+        "temperature": 0.5,
+        "stop": []
+    }
+
+    jsondata = json.dumps(data)
+    res = session.post(url = url, data = jsondata, headers = headers)
+    response_dict = json.loads(res.text.strip())
+    print(response_dict)
+    try:
+        return response_dict['choices'][0]['message']['content'].strip()
+    except:
+        return ""
+
+
+def embedding(text: str) -> List[float]:
+    url = "http://freegpt.club/gptworld_embedding"
+    headers={"Content-Type":"application/json"}
+    session = requests.Session()
+    data = {
+        "model": "text-embedding-ada-002",
+        "input": text
+    }
+
+    jsondata = json.dumps(data)
+    res = session.post(url = url, data = jsondata, headers = headers)
+    response_dict = json.loads(res.text.strip())
+    print(response_dict)
+
+    try:
+        return response_dict['data'][0]['embedding']
+    except:
+        return []
+
