21b1cf09e08f6382e6196e26800af178443578f3
Bokai Xu <bokesyo2000@gmail.com>
2023-04-29 02:21:55 +0800

    add comments to fronend, and update readme

--- game/text_grid/frontend/src/main.ts
+++ game/text_grid/frontend/src/main.ts
@@ -6,7 +6,10 @@ import { io, Socket } from "socket.io-client";
 
 const API_ROOT = "http://localhost:5001";
 
+// the ratio betweem game height and width
+const HEIGHT_WIDTH_RATIO = 3 / 4;
 
+// I. UI Logging Implementation
 const messagesElement = document.getElementById('messages')!;
 
 // socket connection to backend server
@@ -22,7 +25,7 @@ socket.on("welcome", (event) => {
   console.log(event.message);
 });
 
-// normal messages
+// normal messages from backend server
 socket.on("server_message", (event) => {
   console.log("Received message:", event);
   
@@ -42,8 +45,6 @@ socket.on("server_message", (event) => {
     messageElement.innerHTML = `<span style="font-weight: bold; color: ${randomColor}">${message.sender}</span>: ${message.text}`;
     messagesElement.appendChild(messageElement);
   });
-
-
 });
 
 socket.on("disconnect", () => {
@@ -56,22 +57,27 @@ window.onbeforeunload = function() {
 };
 
 
+// II. Game Interface Implementation
+const canvas = document.getElementById('game') as HTMLCanvasElement;
+const ORIGIN_X = 100;
+const ORIGIN_Y = 100;
 
 class ObjectSprite extends Phaser.GameObjects.Sprite {
-  tooltip?: Phaser.GameObjects.Text;
-  display_name?: string;
+  // This class is applicable for all objects (not including areas), it extends the Sprite class to store more information we need
+
+  // add an attribute to store the tooltip
+  tooltip?: Phaser.GameObjects.Text; 
+
+  // add an attribute to store the name displayed in the tooltip
+  display_name?: string; 
 
   constructor(scene: Phaser.Scene, x: number, y: number, texture: string) {
     super(scene, x, y, texture);
   }
 }
 
-
-const canvas = document.getElementById('game') as HTMLCanvasElement;
-const ORIGIN_X = 100;
-const ORIGIN_Y = 100;
-
 function showTooltip(sprite: ObjectSprite) {
+  // once mouse is on a sprite, this function will be called
   if (sprite.tooltip) {
     // tooltip exists, nothing to do
   } else {
@@ -88,23 +94,22 @@ function showTooltip(sprite: ObjectSprite) {
       );
     sprite.tooltip = text;
   }
-
-
 }
 
 function hideTooltip(sprite: ObjectSprite) {
+  // once mouse leaves the sprite, this function will be called
   if (sprite.tooltip) {
     let text = sprite.tooltip;
     text.destroy();
     sprite.tooltip = undefined;
   } else {
-    // do nothing
+    // if there is no tooltip displayed, do nothing
   }
 }
 
-
 function loadIcon(name: string, scene: Scene) {
-  let url = API_ROOT+"/text_to_icon?name="+name;
+  // given a sprite name, this function will fetch an icon from backend and store it in Phaser scene, this function is non-blocking, so the icon of sprites will be updated later
+  let url = API_ROOT + "/text_to_icon?name="+name;
   fetch(url)
     .then(response => response.blob())
     .then(blob => {
@@ -116,7 +121,9 @@ function loadIcon(name: string, scene: Scene) {
 }
 
 async function loadTile(name: string, scene: Scene) {
-  let url = API_ROOT+"/text_to_tile?name="+name;
+  // given a area name, this funtion will fetch a tile from backend and store it in Phase scene, 
+  // this function is async, which means we expect it to block until the fetching is completed, so the tile texture will be determined at this time, with no further changes
+  let url = API_ROOT + "/text_to_tile?name="+name;
   const response = await fetch(url);
   const blob = await response.blob();
   const imageUrl = URL.createObjectURL(blob);
@@ -135,8 +142,8 @@ class GameScene extends Scene {
   }
 
   async load_file(){
-    const response = await fetch(API_ROOT+"/read_environment");
-    // console.log(response);
+    const environment_api = API_ROOT + "/read_environment";
+    const response = await fetch(environment_api);
     const data = await response.json();
     this.mydata = data['message'];
     return Promise.resolve();
@@ -152,25 +159,30 @@ class GameScene extends Scene {
   }
 
   async preload() {
-    
+    // nothing to do
   }
 
   async create_scene(){
+    // render all areas, without any further updates
+
+    // first load environment json
     await this.load_file();
 
     this.lastTime = 0;
     this.gridGraphics = this.add.graphics();
 
-    // console.log(this.mydata.areas);
+    // control the size of each pixel
+    this.gridGraphics.lineStyle(5, 0xEEE8CD, 1.0); 
 
-    this.gridGraphics.lineStyle(5, 0xEEE8CD, 1.0);
+    // add a border for the whole world
     this.gridGraphics.strokeRect(
       ORIGIN_X - this.squareSize / 2 - 4,
       ORIGIN_Y - this.squareSize / 2 - 4,
       this.mydata.size[0] * this.squareSize + 8,
       this.mydata.size[1] * this.squareSize + 8
     );
-
+    
+    // render all areas (no further updates)
     let areas_key = Object.keys(this.mydata.areas)
     for (let i=0; i < areas_key.length; i++){
       let area_key = areas_key[i]
@@ -206,43 +218,44 @@ class GameScene extends Scene {
   }
 
   async place_objects(){
+    // update objects at a given time
     await this.load_file();
-    // Loop through each object in mydata.objects
 
+    // Loop through each object in mydata.objects
     let objects_key = Object.keys(this.mydata.objects)
     for (let i=0; i < objects_key.length; i++){
       let object_key = objects_key[i]
       let obj = this.mydata.objects[object_key];
-
       let obj_x = obj.location[0] + this.mydata.areas[obj.eid].location[0][0]
       let obj_y = obj.location[1] + this.mydata.areas[obj.eid].location[0][1]
 
-      // console.log("obj_x, obj_y", obj_x, obj_y)
       // Check if the object already exists in the scene
       let existingObj = this.children.getByName(obj.id);
       if (existingObj) {
         let existingObjSprite = existingObj as ObjectSprite;
+
         // If the object already exists, update its location
         existingObjSprite.x = ORIGIN_X + obj_x * this.squareSize - this.squareSize / 2;
         existingObjSprite.y = ORIGIN_Y + obj_y * this.squareSize - this.squareSize / 2;
 
+        // update the texture (because we use non-await function to load the icon, so we need to update in latter steps)
         existingObjSprite.setTexture(obj.name);
-
       } else {
 
+        // here we use non-await function call to load icons for objects
         loadIcon(obj.name, this);
-
         let newObj = this.add.sprite(
           ORIGIN_X + obj_x * this.squareSize  - this.squareSize / 2,
           ORIGIN_Y + obj_y * this.squareSize  - this.squareSize / 2,
           obj.name
         ) as ObjectSprite;
 
+        // control the size of sprites in the scene
         newObj.setScale(this.squareSize / 40);
         newObj.setName(object_key);
         newObj.display_name = obj.name;
 
-        // tooltip listener
+        // add tooltip listener
         newObj.setInteractive();
         newObj.on('pointerover', function () {
           showTooltip(newObj);
@@ -250,18 +263,14 @@ class GameScene extends Scene {
         newObj.on('pointerout', function () {
           hideTooltip(newObj);
         });
-
-
       }
     }
   }
 
   async create() {
-    const width = Math.max(window.innerWidth * 0.7);
-    const height = Math.max(window.innerWidth * 0.75 * 0.7);
-    canvas.style.width = width + 'px';
-    canvas.style.height = height + 'px';
+    resize_canvas();
 
+    // load environment json and tile at start (blocking function call)
     await this.load_file();
     await this.load_tile();
     this.lastTime = 0.0;
@@ -272,10 +281,11 @@ class GameScene extends Scene {
 
   // Use the update() function to update the square's locationition every 1 second to the left
   update(time: integer) {
+
       // Set the scene's time event to update the square's locationition every 1 second to the left
       const lastTime = this.lastTime as integer;
       if (time > lastTime + 1000) {
-        console.log("Update")
+        
         this.place_objects()
         this.lastTime = time
       }
@@ -286,7 +296,6 @@ const config = {
   type: WEBGL,
   zoom:Phaser.AUTO,
   canvas,
-
   physics: {
     default: 'arcade',
     arcade: {
@@ -297,13 +306,18 @@ const config = {
   scene: [GameScene]
 };
 
-var game = new Game(config);
+function resize_canvas() {
+  // resize the canvas according to current window size
+  const width = Math.max(window.innerWidth * 0.7);
+  const height = Math.max(window.innerWidth * HEIGHT_WIDTH_RATIO * 0.7);
+  canvas.style.width = width + 'px';
+  canvas.style.height = height + 'px';
+}
 
 // resize the game object as windows resize
 window.addEventListener('resize', function () {
-  const width = Math.max(window.innerWidth * 0.7);
-  const height = Math.max(window.innerWidth * 0.75 * 0.7);
-  canvas.style.width = width + 'px';
-  canvas.style.height = height + 'px';
+  resize_canvas();
 });
+
+new Game(config);
 
